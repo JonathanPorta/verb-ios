9db482d97222184fbb3acd589c97ffad26839a4a
Jonathan Porta <jonathan@jonathanporta.com>
2014-08-29 05:28:20 -0600

    Activity view uses actual activity models now.

--- ActivityModel.swift
+++ ActivityModel.swift
@@ -9,5 +9,20 @@
 import Foundation
 
 class ActivityModel {
-    
+  var userId: NSInteger
+  var messageId: NSInteger
+  var body: NSString
+  var acknowledgedAt: NSDate
+  var createdAt: NSDate
+  var updatedAt: NSDate
+  
+  init(userId:Int, messageId:Int, body:String, acknowledgedAt:NSDate, createdAt:NSDate, updatedAt:NSDate) {
+    self.userId = userId
+    self.messageId = messageId
+    self.body = body
+    self.acknowledgedAt = acknowledgedAt
+    self.createdAt = createdAt
+    self.updatedAt = updatedAt
+  }
+
 }
--- Verb/ActivityViewController.swift
+++ Verb/ActivityViewController.swift
@@ -10,17 +10,47 @@ import Foundation
 
 class ActivityViewController: UITableViewController {
 
+  var activityModelList: NSMutableArray = []
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    //var activeSession = FBSession.activeSession()
-    //if activeSession.isOpen {
-    //  self.performSegueWithIdentifier("ShowLogin", sender: self)
-    //}
+    loadData()
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+  }
+  
+  override func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
+    return 1
+  }
+  
+  override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) ->
+    Int {
+     return self.activityModelList.count
+    }
+  
+  override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+    let CellIndentifier: NSString = "ListPrototypeCell"
+    var cell : UITableViewCell = tableView.dequeueReusableCellWithIdentifier(CellIndentifier) as UITableViewCell
+    var activityModel: ActivityModel = self.activityModelList.objectAtIndex(indexPath.row) as ActivityModel
+    cell.textLabel.text = activityModel.body
+    return cell
+  }
+  
+  func loadData() {
+    // Load some sample data.
+    var activityModel1 = ActivityModel(userId: 0, messageId: 0, body: "You poked Austin!", acknowledgedAt: NSDate(), createdAt: NSDate(), updatedAt: NSDate())
+    var activityModel2 = ActivityModel(userId: 0, messageId: 1, body: "You nudged Ricardo!", acknowledgedAt: NSDate(), createdAt: NSDate(), updatedAt: NSDate())
+    var activityModel3 = ActivityModel(userId: 0, messageId: 2, body: "You hugged Jessica!", acknowledgedAt: NSDate(), createdAt: NSDate(), updatedAt: NSDate())
+    var activityModel4 = ActivityModel(userId: 0, messageId: 3, body: "Jessica Hugged you!", acknowledgedAt: NSDate(), createdAt: NSDate(), updatedAt: NSDate())
+    
+    self.activityModelList.addObject(activityModel1)
+    self.activityModelList.addObject(activityModel2)
+    self.activityModelList.addObject(activityModel3)
+    self.activityModelList.addObject(activityModel4)
+    
   }
 }
