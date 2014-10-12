53186f5ee17bf1790b64a4bb26059c392d89a075
Jonathan Porta <jonathan@jonathanporta.com>
2014-10-11 20:05:13 -0700

    Always sign and upload for testing

--- scripts/sign-build.sh
+++ scripts/sign-build.sh
@@ -2,12 +2,12 @@
 
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
   echo "This is a pull request. No deployment will be done."
-  exit 0
+#  exit 0
 fi
 
 if [[ "$TRAVIS_BRANCH" != "master" ]]; then
   echo "Testing on a branch other than master. No deployment will be done."
-  exit 0
+#  exit 0
 fi
 
 # Thanks @djacobs https://gist.github.com/djacobs/2411095
