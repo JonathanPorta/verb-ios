c80c84155deb255b5bdf4686342ad6cedbf0b8ae
Jonathan Porta <jonathan@jonathanporta.com>
2014-11-23 13:15:28 -0800

    Tell xctool to explicitly sign an identity

--- scripts/run-locally.sh
+++ scripts/run-locally.sh
@@ -8,7 +8,7 @@ echo "Settings Bundle Data"
 ./scripts/set-bundle.sh
 
 echo "Build"
-xctool -workspace Verb.xcworkspace -scheme Verb-travis -sdk iphoneos -configuration Release OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO
+xctool -workspace Verb.xcworkspace -scheme Verb-travis -sdk iphoneos -configuration Release OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO CODE_SIGN_IDENTITY="$DEVELOPER_NAME"
 
 echo "Sign and Upload Build"
 ./scripts/sign-build.sh
