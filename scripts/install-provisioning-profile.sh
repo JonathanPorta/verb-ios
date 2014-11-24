18bf76181e76ae73ea2a59840384d3f9c13460e2
Jonathan Porta <jonathan@jonathanporta.com>
2014-11-23 17:27:24 -0800

    Need to double quote paths with spaces

--- scripts/install-provisioning-profile.sh
+++ scripts/install-provisioning-profile.sh
@@ -5,10 +5,10 @@ set -e
 # Figure out the UUID of our provisioning profile - http://stackoverflow.com/questions/10398456/can-an-xcode-mobileprovision-file-be-installed-from-the-command-line/10775025#10775025
 export PROFILE_UUID=`grep UUID -A1 -a ./scripts/profile/$PROFILE_NAME.mobileprovision | grep -io "[-A-Z0-9]\{36\}"`
 
-PROFILE_DIRECTORY=$HOME/Library/MobileDevice/Provisioning\ Profiles
+PROFILE_DIRECTORY="$HOME/Library/MobileDevice/Provisioning Profiles"
 INSTALLED_PROFILE="$PROFILE_DIRECTORY/$PROFILE_UUID.mobileprovision"
 
 # Put the provisioning profile in place
 echo "Installing provisioning profile $PROFILE_NAME as $INSTALLED_PROFILE"
-mkdir -p $PROFILE_DIRECTORY
+mkdir -p "$PROFILE_DIRECTORY"
 cp "./scripts/profile/$PROFILE_NAME.mobileprovision" "$INSTALLED_PROFILE"
